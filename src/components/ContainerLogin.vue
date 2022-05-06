9cf844767a250464c611f547a961eff976758006
mariagerotti <mariaeduarda.gerotti@gmail.com>
2022-05-06 09:10:05 -0300

    obrigada prof eduardo pela ajuda

--- src/components/ContainerLogin.vue
+++ src/components/ContainerLogin.vue
@@ -3,5 +3,53 @@ export default {};
 </script>
 
 <template>
-  <article class="block">alooo</article>
+  <section class="container-login">
+    <div class="login">
+      <div class="icon-user">
+        <span class="material-symbols-outlined"> account_circle </span>
+      </div>
+      <input type="text" />
+      <input type="text" />
+    </div>
+    <div class="entrar">Entrar</div>
+  </section>
 </template>
+
+<style>
+.container-login {
+  min-width: 10%;
+  height: 90%;
+  padding: 20px;
+  margin: 36px;
+  flex: 1;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login,
+.entrar {
+  background: rgba(255, 255, 255, 0.28);
+  margin: 10px;
+  height: 500px;
+  width: 350px;
+}
+
+.login {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.login input {
+  margin: 10px 20px;
+  height: 35px;
+  border: 0;
+}
+
+.icon-user {
+  width: 50px;
+  height: 50px;
+}
+</style>
